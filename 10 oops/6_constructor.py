2634a27ddf0185030c0984149187caf37a50ebbe
priyanshu ranjan <priyanshu82ranjan48@gmail.com>
2025-08-01 16:53:29 +0530

    constructor learning
    learned constructor

---
+++ 10 oops/6_constructor.py
@@ -0,0 +1,23 @@
+class College:
+    Name = "SVM"
+    Location = "Bannergatta"
+    Rating = 4
+    Classes = 2
+    Crush = "Shree"
+
+    @staticmethod
+    def love():
+        print(f"i liked talking to {College.Crush} ")  # using static method to talk about my crush 
+
+    def __init__(self, herfullname):
+        self.herfullname = herfullname  # hehe i won’t reveal her full name u noob anyone reading this
+        
+
+College.love()  # calling directly like opening up my heart in public
+
+# print(College.herfullname) 
+#  this will give error because herfullname belongs to a specific object, not the class
+
+forgetherformyfuturewife = College("ranjan")  # made a cute object just for her 
+print(forgetherformyfuturewife.herfullname)  # revealing only to me what's stored in the heart 
+
